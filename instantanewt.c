9109b7e4ee21ddf8e0528fdae46b273ef88519c4
TDIAKITE23 <tiemokodjandiakite@gmail.com>
2023-03-21 04:20:55 +0100

    idées pour les 2  dernières questions de l'exo 5

--- instantanewt.c
+++ instantanewt.c
@@ -65,5 +65,76 @@ char* blobWorkTree(WorkTree* wt){
 
 /*Creer recursivement tous les instantanes du WorkTree et enfin l'instantane de celui-ci*/
 char * saveWorkTree(WorkTree* wt, char* path){
-	return "aie";
+	//cette fonction est probablement incorrecte
+	for(int i=0;i<wt->n;i++){
+		WorkFile * WF=wt->tab[i];
+		if( is_regular_file(WF->name)){
+			blobFile(WF->name);
+			int mode=getChmod(path); 
+			setMode(mode,path);
+		}else{
+			if(is_directory(WF->name)){
+				List * l=listdir(WF->name);
+
+				WorkFile* wf = NULL;
+				WorkTree* newWt = initWorkTree();
+
+				while(*l){
+					wf = stwf((*l)->date);
+					appendWorkTree(newWt, wf->name, wf->hash, wf->mode);
+					freeWorkFile(wf);
+				}
+
+				saveWorkTree(newWt,path);
+				int mode=getChmod(path); 
+				setMode(mode,path);
+			}
+		}
+	}
+
+	char *hash=blobWorkTree(wt);
+
+	return hash;
+
+
+
+	//return "aie";
+}
+
+void restoreWorkTree(WorkTree* wt, char* path){
+	//cette fonction est problement incorrecte 
+	for(int i=0;i<wt->n;i++){
+		WorkFile * WF=wt->tab[i];
+
+		//cas sans ".t"
+		if(){
+			cp(path,WF->name);
+			setMode(Wf->mode,path);
+			//donner noms et autorisations j'ai pas bien compris
+		}
+		//cas avec ".t"
+		if(){
+
+			List * l=listdir(WF->name);
+
+			WorkFile* wf = NULL;
+			WorkTree* newWt = initWorkTree();
+
+			while(*l){
+				wf = stwf((*l)->date);
+				appendWorkTree(newWt, wf->name, wf->hash, wf->mode);
+				freeWorkFile(wf);
+			}
+			char newpath[256];
+			strcpy(newpath,path)
+			strcat(newpath,"/");
+			strcat(newpath,WF->name);
+			restoreWorkTree(newWt,newpath);
+
+		}
+
+	}
+
+
+
 }
