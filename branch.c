89eeb0528bdabe7dee2d642ff7d8264b28123766
TDIAKITE23 <tiemokodjandiakite@gmail.com>
2023-04-02 18:03:24 +0200

    branch.c

---
+++ branch.c
@@ -0,0 +1,76 @@
+#include "branch.h"
+#include "file_utility.h"
+
+void initBranch(){
+	FILE*F=fopen(".current_branch","w");
+	fputs("master",f);
+	fclose(f);
+}
+
+int branchExists(char * branch){
+<<<<<<< HEAD
+	List*refs=listdir(".refs");
+	
+	return searchList(refs,branch) != NULL;
+=======
+	int res = 0;
+
+	List*refs=listdir(".refs");
+
+	if (searchList(refs,branch) != NULL) {
+		res = 1;
+	}
+
+	freeList(refs);
+	return res;
+>>>>>>> 5d0d7a702f593cffb0c28b22483dc67617278ded
+}
+void createBranch(char * branch){
+	char *hash=getRef("HEAD");
+	createUpdateRef(branch,hash);
+}
+char * getCurrentBranch(){
+	FILE*f=fopen(".current_branch","r");
+	char*buff=malloc(sizeof(char)*100);
+	fscanf(f,"%s",buff);
+	return buff;
+}
+
+char *hashToPathCommit(char * hash){
+	char * buff=malloc(sizeof(char)*100);
+	sprintf(buff,"%s.c",hashToPath(hash));
+	return buff;
+}
+
+void printBranch(char* branch){
+	//recupérer le fichier contenant la référence(le hash) du commit
+	char*commit_hash=getRef(branch);
+	//conversion en commit
+	Commit * c=ftc(hashToPathCommit(commit_hash));
+	while(c != NULL){
+		if (commitGet(c,"message") != NULL){
+			printf("%s -> %s\n",commit_hash,commitGet(c,"message"));
+		}else{
+			printf("%s \n",commit_hash);
+		}
+		if(commitGet(c,"predecessor") != NULL){
+			commit_hash=commitGet(c,"predecessor");
+			c=ftc(hashToPathCommit(commit_hash));
+		}else{
+			c=NULL;
+		}
+	}
+}
+
+List * branchList(char * branch){
+	List*l=initList();
+	char * commit_hash=getRef(branch);
+	Commit * c=ftc(hashToPathCommit(commit_hash));
+	while( c != NULL ){
+		Cell*cellule=buildCell(commit_hash);
+		insertFirst(l,cellule);
+		commit_hash=commitGet(c,"predecessor");
+		c=ftc(hashToPathCommit(commit_hash));
+	}
+	return l;
+}
